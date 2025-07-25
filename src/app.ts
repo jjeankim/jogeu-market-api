1212daaa468da5dedd923b23b4d88692bec0facd
Youngsun33 <heoyoungsun33@gmail.com>
2025-07-25 12:09:48 +0900

    Feat : 브랜드 생성, 조회 기능 구현 Feat : 상품 생성, 조회, 상세조회 기능구현

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
+import ProductRouter from "./routes/productRouter";
+import brandRouter from "./routes/brandRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("ok");
 });
+
+app.use("/api/brands", brandRouter);
+app.use("/api/products", ProductRouter);
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
---
+++ src/controllers/brandController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createBrand = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({
+        message: "브랜드 이름은 필수 입력입니다.",
+      });
+    }
+
+    const newBrand = await prisma.brands.create({
+      data: {
+        name,
+      },
+    });
+
+    console.log(newBrand);
+
+    return res.status(201).json({
+      message: "✅ 브랜드가 성공적으로 등록되었습니다.",
+      products: newBrand,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+};
+
+export const getAllBrand = async (req: Request, res: Response) => {
+  try {
+    const findAllBrand = await prisma.brands.findMany();
+
+    console.log(findAllBrand);
+
+    return res.status(200).json({
+      message: "✅ 브랜드가 성공적으로 조회되었습니다.",
+      products: findAllBrand,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+};
