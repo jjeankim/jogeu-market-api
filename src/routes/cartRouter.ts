46b88129935a6c59cb1f03be52dac3802ac90a5d
Youngsun33 <heoyoungsun33@gmail.com>
2025-07-25 16:46:34 +0900

    feat : cart crud 구현 완료

--- src/routes/cartRouter.ts
+++ src/routes/cartRouter.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { createCart } from "../controllers/cartController";
+import {
+  createCart,
+  getCart,
+  patchCart,
+  deleteCart,
+} from "../controllers/cartController";
 
 const cartRouter = Router();
 cartRouter.post("/", createCart);
+cartRouter.get("/", getCart);
+cartRouter.patch("/:id", patchCart);
+cartRouter.delete("/:id", deleteCart);
 
 export default cartRouter;
