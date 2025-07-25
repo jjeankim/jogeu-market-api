035ee33f21a34182b3000e2214c1fce4215adfe1
dev-suz <return@knou.ac.kr>
2025-07-25 14:14:19 +0900

    feat: 인증(회원가입/로그인) 로직 및 파일 구조 개선

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import authRouter from "./routes/authRouter";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("ok");
