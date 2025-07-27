fb4b4f3e95e906de1ffcfac252a8d1f801d2db50
jjeankim <miyukikj35@gmail.com>
2025-07-27 16:33:10 +0900

    Feat: cors 설정

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
 import express, { Request, Response } from "express";
 import prisma from "./lib/prisma";
-import  productReviewRouter  from "./routes/productReviewRouter";
+import productReviewRouter from "./routes/productReviewRouter";
+import cors from "cors";
 
 import dotenv from "dotenv";
 import authRouter from "./routes/authRouter";
 import ProductRouter from "./routes/productRouter";
 import brandRouter from "./routes/brandRouter";
 import cartRouter from "./routes/cartRouter";
-import  orderRouter from "./routes/orderRouter";
+import orderRouter from "./routes/orderRouter";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
-app.use("/api/products/:id/reviews",productReviewRouter)
+app.use("/api/products/:id/reviews", productReviewRouter);
 app.use("/api/auth", authRouter);
 
 app.use("/api/brand", brandRouter);
 app.use("/api/product", ProductRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/orders",orderRouter)
-
+app.use("/api/orders", orderRouter);
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
