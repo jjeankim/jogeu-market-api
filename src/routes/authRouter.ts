54814a5a08c924bc636f24e3a6036a559748e330
jjeankim <miyukikj35@gmail.com>
2025-07-31 22:09:28 +0900

    Fix: refreshToken 갱신을 위해 로직 수정

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { login, refreshToken, signup } from "../controllers/authController";
+import {
+  login,
+  logout,
+  refreshToken,
+  signup,
+} from "../controllers/authController";
 
 const Authrouter = Router();
 
 Authrouter.post("/signup", signup);
 Authrouter.post("/login", login);
-Authrouter.post("/tokens", refreshToken);
+Authrouter.post("/refresh", refreshToken);
+Authrouter.post("/logout", logout);
 
 export default Authrouter;
