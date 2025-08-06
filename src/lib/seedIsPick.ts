3a527f7f64f61bc3e3ff07d97d292308caa34e88
dev-suz <return@knou.ac.kr>
2025-08-06 16:21:48 +0900

    fix: import prisma

--- src/lib/seedIsPick.ts
+++ src/lib/seedIsPick.ts
@@ -1,6 +1,4 @@
-// scripts/seedIsPick.ts
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from "../lib/prisma";
 
 async function main() {
   const products = await prisma.product.findMany();
