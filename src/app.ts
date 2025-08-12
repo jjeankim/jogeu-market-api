1341e0cc24b8229bcc84043269d7f81d0f94c557
김진 <miyukikj35@gamil.com>
2025-08-12 16:30:14 +0900

    helmet & compression 적용

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,12 @@ import cookieParser from "cookie-parser";
 import productRouter from "./routes/productRouter";
 import categoryRouter from "./routes/categoryRouter";
 import { errorHandler } from "./middleware/errorHandler";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
+app.use(compression());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -76,5 +80,3 @@ const shutdown = async () => {
 
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
-
-
