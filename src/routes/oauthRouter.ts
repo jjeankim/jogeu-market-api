af1ce1a1a657c773fabda56304a9ce142d3e20e3
jjeankim <miyukikj35@gmail.com>
2025-08-14 20:11:39 +0900

    test

--- src/routes/oauthRouter.ts
+++ src/routes/oauthRouter.ts
@@ -59,7 +59,7 @@ router.post("/auth/kakao", async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", //개발중일때는 false
+      secure: process.env.NODE_ENV === "production", 
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: "/",
